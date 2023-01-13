948c1fa4a672c03545dc7a5264cf36d7e70d3e08
KhakedSobhy22 <khaled.sobhy@easwaaqmisr.com>
2023-01-13 10:04:35 +0200

    extend box shadow and add card shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        card: "rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px",
+      },
       transitionProperty: {
         width: "width",
         margin: "margin",
